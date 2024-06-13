f117098bea69c28fa63f2eb61fd1368469d275ff
EowynWinchester <141828970+EowynWinchester@users.noreply.github.com>
2024-06-13 21:44:52 +0100

    This is a big improvement!  There is a a results list that holds all the information we want to display in the result, and comments are added in a block at the end. Next steps: * elimiate extra quotation marks * incorporate score and house

--- main.py
+++ main.py
@@ -81,58 +81,102 @@ S = positions_lists[5]
 O = positions_lists[6]
 GS = positions_lists[7]
 
-
-# Score each result
+# Initialise score and results list
 score = 0
+name = ""
+e1 = ""
+r1 = None
+e2 = ""
+r2 = None
+e3 = ""
+r3 = None
+e4 = ""
+r4 = None
+e5 = ""
+r5 = None
+comment = ["", "", "", "", "", ""]
+comment_str = ""
+results = [name, e1, r1, e2, r2, e3, r3, e4, r4, e5, r5, comment_str]
 
 # Score each line
 for line in data_lines:
+  # Initialise the entry list and score
   entry = line[1:6]
   score = 0
+  results[0] = line[0]
+
+  # Populate the entries into the results list
+  for i in range (5):
+    results[2*i+1] = entry[i]
 
   # Starting score 10 for playing
   score = score + 10
-  
+
+  # If Thestral Poo - total score 0, game will only be scored up to this entry index
+  game_over = ""
+  for i in range(5):
+    if entry[i] == T:
+      game_over = i
+      score = 0
+  if game_over != "":
+    entry = entry [0:i]
+    results[2*i+2] = "- THESTRAL POO!"
+    comment[i] = "THESTRAL COMMENT"
+
   # For each entry that matches beater bat, +5
-  for guess in entry:
+  for i in range(len(entry)):
     for bat in BB:
-      if guess == bat:
+      if entry[i] == bat:
         score = score + 5
+        results[2*i+2] = "- BEATER'S BAT!"
   
   # For each entry that matches quaffle, +10
-  for guess in entry:
-    if guess == Q:
+  for i in range(len(entry)):
+    if entry[i] == Q:
       score = score + 10
+      results[2*i+2] = "- QUAFFLE!"
       
   # For each entry that matches Goal Post, +5
-  for guess in entry:
+  for i in range(len(entry)):
     for post in GP:
-      if guess == post:
+      if entry[i] == post:
         score = score + 5
+        results[2*i+2] = "- GOAL POST!"
   
   # For each entry that matches Snitch, + 150
-  for guess in entry:
-    if guess == GS:
+  for i in range(len(entry)):
+    if entry[i] == GS:
       score = score + 150
+      results[2*i+2] = "- GOLDEN SNITCH!"
+      comment[i] = "Great catch!  You've got the Golden Snitch! "
+
   
   # For each entry that matches Bludger, - 5
-  for guess in entry:
+  for i in range(len(entry)):
     for bludger in B:
-      if guess == bludger:
+      if entry[i] == bludger:
         score = score - 5
+        results[2*i+2] = "- BLUDGER!"
+        #comment[i] = "Oooh, that hurt!  Need to keep an eye out for those rogue Bludgers!"
   
   
   # If bat comes before bludger, +5 
   # Initialise order_list
   order_list = ["0", "0", "0", "0", "0"]
+  hit_BB = []
+  hit_B = []
+
+  
   # Loop through the entry to find bats and strikes
-  for guess_i in range(5):
+  for guess_i in range(len(entry)):
     for bat_i in range(4):
       if entry[guess_i] == BB[bat_i]:
         order_list[guess_i] = "B"
+        hit_BB.append(guess_i)
     for bludger_i in range(2):
       if entry[guess_i] == B[bludger_i]:
         order_list[guess_i] = "S"
+        hit_B.append(guess_i)
   # Remove any place holders
   order_list = [ele for ele in order_list if ele != "0"]
   # Identify bats and strikes in one string
@@ -141,6 +185,10 @@ for line in data_lines:
   location = order.find("BS")
   if order == "BBSS":
     bonus = 2
+    comment[hit_BB[0]] = "You're ready for those stray Bludgers now!"
+    comment[hit_BB[1]] = "Another beater bat ready to protect your team!"
+    comment[hit_B[0]] = "Great defensive work!"
+    comment[hit_B[1]] = "Those Bludgers can't even get close to you!"
   elif location > -1:
     bonus = 1
     leftover = order[location+2:]
@@ -148,6 +196,10 @@ for line in data_lines:
     second_location = leftover.find("BS")
     if second_location > -1:
       bonus = 2
+      comment[hit_BB[0]] = "You're ready for those stray Bludgers now!"
+      comment[hit_BB[1]] = "Another beater bat ready to protect your team!"
+      comment[hit_B[0]] = "Great defensive work!"
+      comment[hit_B[1]] = "Those Bludgers can't even get close to you!"
   elif location == -1:
     bonus = 0
   else:
@@ -157,13 +209,17 @@ for line in data_lines:
   # If quaffle comes before goal post, + 10
   # Initialise order_list
   order_list = ["0", "0", "0", "0", "0"]
+  hit_Q = ""
+  hit_GP = []
   # Loop through the entry to find quaffle and goal posts
-  for guess_i in range (5):
+  for guess_i in range (len(entry)):
     if entry[guess_i] == Q:
       order_list[guess_i] = "Q"
+      hit_Q = guess_i
     for goalpost_i in range(6):
       if entry[guess_i] == GP[goalpost_i]:
         order_list[guess_i] = "G"
+        hit_GP.append(guess_i)
   # Remove any place holders
   order_list = [ele for ele in order_list if ele != "0"]
   # Identify quaffle and goal posts in one string
@@ -172,35 +228,45 @@ for line in data_lines:
   location = order.find("QG")
   if location > -1:
     score = score + 10
+    comment[hit_Q] = "You've got the Quaffle, now head to the goal!"
+    comment[hit_GP[0]] = "You made it to the goal post and scored!!"
+  else:
+    if len(hit_GP) != 0:
+      comment[hit_GP[0]] = "You're at the goal, but you forgot to get the Quaffle first!"
+
   
   # For each entry that matches Owl, - 5
-  for guess in entry:
+  for i in range(len(entry)):
     for owl in O:
-      if guess == owl:
+      if entry[i] == owl:
         score = score - 5
+        results[2*i+2] = "- OWL!"
+        comment[i] = "Watch out for Errol!  Spit those feathers out and get back in the game!"
         
   # For each entry that matches Scoreboard, + 10
-  for guess in entry:
-    if guess == S:
+  for i in range(len(entry)):
+    if entry[i] == S:
       score = score + 10
+      results[2*i+2] = "- SCOREBOARD!"
+      comment[i] = "Great work - way to bump up that score!"
   
   # If score is negative, score becomes 0 and game over
   if score < 0:
     score = 0
   
-  # If Thestral Poo - total score 0 and game over
-  for guess in entry:
-    if guess == T:
-      score = 0
+  # Turn blanks into misses
+  for i in range (len(entry)):
+    if results[2*i+2] == None:
+      results[2*i+2] = "- MISS"
   
-  #entry.append(score)
-  new_line = [line[0]]
-  for i in range(5):
-    new_line.append(entry[i])
-  print(new_line)
-  new_line.append(score)
-  print(new_line)
-  csv_writer.writerow(new_line)
+  # Create a single comment string
+  for word in comment:
+    comment_str = comment_str + word
+  results[-1] = comment_str
+
+  
+  # Write the results to the csv file
+  csv_writer.writerow(results)
 
 # save it all and shut it down
 data_output.close()
